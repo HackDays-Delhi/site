6647440db5912b9821cf6907d7d9479d9c42a4db
Aryan <aryanbrite@gmail.com>
2026-06-22 16:26:04 +0530

    added touch responce

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
 const prizes = document.querySelectorAll(".prize");
 
 prizes.forEach((prize) => {
-    let isDragging = false;
     let offsetX = 0;
     let offsetY = 0;
+    let dragging = false;
 
-    prize.addEventListener("mousedown", (e) => {
-        isDragging = true;
+    prize.addEventListener("pointerdown", (e) => {
+        dragging = true;
 
         const rect = prize.getBoundingClientRect();
         offsetX = e.clientX - rect.left;
         offsetY = e.clientY - rect.top;
 
-        prize.style.zIndex = Date.now(); // jispe click karo wo upar aa jaye
         prize.style.cursor = "grabbing";
+        prize.style.zIndex = Date.now();
+
+        prize.setPointerCapture(e.pointerId);
     });
 
-    document.addEventListener("mousemove", (e) => {
-        if (!isDragging) return;
+    prize.addEventListener("pointermove", (e) => {
+        if (!dragging) return;
 
         prize.style.left = `${e.clientX - offsetX}px`;
         prize.style.top = `${e.clientY - offsetY}px`;
     });
 
-    document.addEventListener("mouseup", () => {
-        isDragging = false;
+    prize.addEventListener("pointerup", () => {
+        dragging = false;
         prize.style.cursor = "grab";
-    });
-
-    prize.addEventListener("dragstart", (e) => {
-        e.preventDefault();
     });
 });
