0e51f1b8b406978e44aa96c030e9ff3c4608ab0d
Aryan <aryanbrite@gmail.com>
2026-06-28 13:09:58 +0530

    removed grab from prixes and fixed india gate return animation

--- script.js
+++ script.js
@@ -4,44 +4,13 @@ const prize2 = document.getElementById("prize2");
 const indiagate = document.getElementById("indiagate");
 const wave = document.getElementById("wave")
 
-prizes.forEach((prize) => {
-    let offsetX = 0;
-    let offsetY = 0;
-    let dragging = false;
-
-    prize.addEventListener("pointerdown", (e) => {
-        dragging = true;
-
-        const rect = prize.getBoundingClientRect();
-        offsetX = e.clientX - rect.left;
-        offsetY = e.clientY - rect.top;
-
-        prize.style.cursor = "grabbing";
-        prize.style.zIndex = Date.now();
-
-        prize.setPointerCapture(e.pointerId);
-    });
-
-    prize.addEventListener("pointermove", (e) => {
-        if (!dragging) return;
-
-        prize.style.left = `${e.clientX - offsetX}px`;
-        prize.style.top = `${e.clientY - offsetY}px`;
-    });
-
-    prize.addEventListener("pointerup", () => {
-        dragging = false;
-        prize.style.cursor = "grab";
-    });
-});
-
 where.onclick = () => {
     wave.classList.remove("wavenew")
     prize2.classList.remove("rightr");
     prize3.classList.remove("rightr");
     prize1.classList.remove("rig1r");
     prize4.classList.remove("rig1r");
-    
+    indiagate.classList.remove("aniir");
     prize2.classList.add("right");
     prize3.classList.add("right");
     prize1.classList.add("rig1");
@@ -57,7 +26,7 @@ prizee.onclick = () => {
     prize3.classList.add("rightr");
     prize1.classList.add("rig1r");
     prize4.classList.add("rig1r");
-    indiagate.classList.remove("aniir");
+    indiagate.classList.add("aniir");
     prize2.classList.remove("right");
     prize3.classList.remove("right");
     prize1.classList.remove("rig1");
