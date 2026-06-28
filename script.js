d5ca53cd7de1ff36870a46aa8019043d2e817c2d
Aryan <aryanbrite@gmail.com>
2026-06-28 13:17:46 +0530

    now the india gate gowes too

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const where = document.getElementById("where");
 const prize2 = document.getElementById("prize2");
 const indiagate = document.getElementById("indiagate");
 const wave = document.getElementById("wave")
+const when = document.getElementById("when")
 
 where.onclick = () => {
     wave.classList.remove("wavenew")
@@ -32,6 +33,22 @@ prizee.onclick = () => {
     prize1.classList.remove("rig1");
     prize4.classList.remove("rig1");
     indiagate.classList.remove("anii");
+    wave.classList.add("wavenew")
+}
+
+when.onclick = () => {
+    wave.classList.remove("wavenew")
+    prize2.classList.remove("rightr");
+    prize3.classList.remove("rightr");
+    prize1.classList.remove("rig1r");
+    prize4.classList.remove("rig1r");
+    indiagate.classList.add("aniir");
+    indiagate.classList.remove("anii");
+    prize2.classList.add("right");
+    prize3.classList.add("right");
+    prize1.classList.add("rig1");
+    prize4.classList.add("rig1");
+    indiagate.classList.add("anii");
     wave.classList.add("wavenew")
 }
 
