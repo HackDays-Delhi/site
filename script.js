e6ecd5afeb7c6c2ebf4993e721b2831289e9688f
Aryan <aryanbrite@gmail.com>
2026-07-02 23:00:47 +0530

    adding animations to sponsors

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const indiagate = document.getElementById("indiagate");
 const wave = document.getElementById("wave");
 const when = document.getElementById("when");
 const time1 = document.getElementById("time1");
+const timo = document.getElementById("timo")
 function countdown() {
     const now = new Date();
     const event = new Date("2026-07-25T09:00:00");
@@ -52,7 +53,7 @@ where.onclick = () => {
     timo10.classList.remove("show");
     timo11.classList.remove("show");
     timo12.classList.remove("show");
-    container.classList.remove("show")
+    container.classList.remove("show");
 
 }
 
@@ -118,7 +119,8 @@ when.onclick = () => {
     timo10.classList.remove("show");
     timo11.classList.remove("show");
     timo12.classList.remove("show");
-    container.classList.remove("show")
+    container.classList.remove("show");
+    
     
 }
 
@@ -147,6 +149,40 @@ timeline.onclick = () => {
     timo11.classList.add("show");
     timo12.classList.add("show");
     cam.classList.add("show");
-    container.classList.add("show")
+    container.classList.add("show");
+    timo.textContent = "Timeline";
+    
     }
+
+
+sponsors.onclick = () => {
+    wave.classList.remove("wavenew");
+    indiagate.classList.remove("add");
+    prize2.classList.add("dis");
+    prize3.classList.add("dis");
+    prize1.classList.add("dis");
+    prize4.classList.add("dis");
+    wave.classList.add("wavenew");
+    timel.classList.remove("show");
+    time1.classList.remove("show");
+    when1.classList.remove("show");
+    timo.classList.add("show");
+    timo1.classList.remove("show");
+    timo2.classList.remove("show");
+    timo3.classList.remove("show");
+    timo4.classList.remove("show");
+    timo5.classList.remove("show");
+    timo6.classList.remove("show");
+    timo7.classList.remove("show");
+    timo8.classList.remove("show");
+    timo9.classList.remove("show");
+    timo10.classList.remove("show");
+    timo11.classList.remove("show");
+    timo12.classList.remove("show");
+    cam.classList.add("show");
+    container.classList.add("show");
+    timo.textContent = "Sponsors";
+    spon1.classList.add("show")
+    
+}
 
